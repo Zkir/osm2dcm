7367f4931a8213ee781d39b352614312c566bd3d
Zkir <mekillot@yandex.ru>
2011-08-16 00:12:56 +0400

    Беларусь с другого источника

--- osm2dcm/get.bat
+++ osm2dcm/get.bat
@@ -22,20 +22,17 @@ call getbz2 finland	 http://download.geofabrik.de/osm/europe/finland.osm.bz2
 call getbz2 estonia      http://download.geofabrik.de/osm/europe/estonia.osm.bz2
 call getbz2 latvia       http://download.geofabrik.de/osm/europe/latvia.osm.bz2
 call getbz2 lithuania    http://download.geofabrik.de/osm/europe/lithuania.osm.bz2
+call getbz2 cyprus       http://download.geofabrik.de/osm/europe/cyprus.osm.bz2
 
 rem ================================================================================
 rem Беларусь, с фтп
 rem ================================================================================
 
-curl ftp://188.40.19.246/osm/dumps/belarus.current.preprocessed.osm.bz2 >D:\OSM\osm_data\belarus.osm.bz2
-bunzip2.exe D:\OSM\osm_data\belarus.osm.bz2 --force
+rem curl ftp://188.40.19.246/osm/dumps/belarus.current.preprocessed.osm.bz2 >D:\OSM\osm_data\belarus.osm.bz2
+rem bunzip2.exe D:\OSM\osm_data\belarus.osm.bz2 --force
 
-
-
-
-
-
-
-
-
+curl http://maps-by.googlecode.com/files/belarus.7z >D:\OSM\osm_data\belarus.7z
+7za e D:\OSM\osm_data\belarus.7z -oD:\OSM\osm_data
+del D:\OSM\osm_data\belarus.osm
+rename D:\OSM\osm_data\77777777.osm belarus.osm
 
