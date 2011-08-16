cb57eb963f187b6bfee0af12a54b6d2d508f2a38
Zkir <mekillot@yandex.ru>
2011-08-17 01:53:00 +0400

    Улучшения по результатам обсуждения - пред. версия, чейджесет, раскраска

--- ChangeTracker.site/changetracker.php
+++ ChangeTracker.site/changetracker.php
@@ -25,9 +25,11 @@
         $strsql=" WHERE MapID='$MapId' and  IsNull(ApprovedUser,'')<>''";
         break;
     case 2:
-        $strsql=" WHERE MapID='$MapId' and IsNull(ApprovedUser,'')=''";
+        $strsql=" WHERE MapID='$MapId' and IsNull(ApprovedUser,'')='' and ChangeDescription like '".iconv('UTF-8','Windows-1251', "%удале%")."'";
         break;
    }
+  $strsql=$strsql." and ChangeDate > DATEADD (month, -1, CONVERT (date,current_timestamp)) ";
+ 
   $strsql="SELECT * FROM WayLog ".$strsql." order by ChangeDate desc";
 
 
@@ -42,6 +44,11 @@
 
 
   echo "<html>\n";
+  echo '<head>';
+  echo '<title>Свежие правки ('.$MapId.')</title>';
+  echo '<link rel="stylesheet" type="text/css" href="style.css" />';
+  echo '</head>';
+  echo '<body>';
   echo '
   <h1>Свежие правки ('.$MapId.')</h1>
   <p><a href="'.$g_SelfUrl.'/changetracker.php?map_id='.$MapId.'&mode=0">Все</a> -
@@ -55,9 +62,10 @@
     <td><b>Name</b></td>
     <td><b>Ref</b></td>
     <td><b>Номер версии</b></td>
+    <td><b>Пред. версия</b></td>
     <td><b>Что случилось</b></td>
     <td><b>Кто изменил</b></td>
-
+    <td><b>В пакете правок</b></td>
      <!--<td><b>Номер одобренной версии</b></td> -->
     <td><b>Кто одобрил</b></td>
     <td><b>Когда</b></td>
@@ -66,20 +74,23 @@
 
   while (($row = mssql_fetch_array($result)))
     {
-      echo "  <tr>\n";
+      echo '  <tr class="g'.$row['DangerStatus'].'">'."\n";
       echo '
        <td> '. date("d.m.Y H:i", strtotime($row['ChangeDate'])).'</td>
-       <td><a href="http://www.openstreetmap.org/browse/way/'.$row['WayID'].'/history">'
+       <td><a href="http://www.openstreetmap.org/browse/way/'.$row['WayID'].'">'
       .$row['WayID'].'</a> </td>
         <td> '
         .$row['Highway'].' </td>
         <td> '.$row['Name'].'</td>
         <td> '.$row['Ref'].'</td>
         <td> '.$row['CurrentVersion'].'</td>
+        <td> '.$row['PreviousVersion'].'</td>
+        
         <!-- <td> '.$row['ChangeDescription'].'</td> -->
         <td> '.iconv('Windows-1251', 'UTF-8', $row['ChangeDescription']).'</td>
-        <td> <a href="http://www.openstreetmap.org/user/'.$row['ChangeUser'].'">'
+        <td> <a href="http://www.openstreetmap.org/user/'.$row['ChangeUser'].'/edits">'
           .$row['ChangeUser'].'</a></td>
+        <td> <a href="http://www.openstreetmap.org/browse/changeset/'.$row['Changeset'].'" >'.$row['Changeset'].'</a></td>
         <!-- <td> '.$row['ApprovedVersion'].'-</td>   -->
         <td> '.$row['ApprovedUser'].'</td>';
 
@@ -91,6 +102,7 @@
     }
 
   echo "</table>\n";
+  echo '</body>';
   echo "</html>\n";
 
   mssql_free_result($result);
