dedb4e9c9466d91b455569528c35a2c60292fe1a
Zkir <mekillot@yandex.ru>
2011-08-29 00:43:32 +0400

    1. Постепенный переход на новый урл выгрузок с гислаб 2. Беларусь без препроцессинга

--- osm2dcm/get.bat
+++ osm2dcm/get.bat
@@ -6,12 +6,15 @@ rem ============================================================================
 
 rem call getbz2 russia      http://data.gis-lab.info/osm/russia/rus.osm.bz2
 call getbz2 russia      http://data.gis-lab.info/osm_dump/dump/latest/RU.osm.bz2
-call getbz2 ukraine     http://data.gis-lab.info/osm/ukraine/ukraine.osm.bz2
-call getbz2 georgia     http://data.gis-lab.info/osm/georgia/georgia.osm.bz2
-call getbz2 armenia     http://data.gis-lab.info/osm/armenia/armenia.osm.bz2
+call getbz2 ukraine     http://data.gis-lab.info/osm_dump/dump/latest/UA.osm.bz2
+call getbz2 kazakhstan  http://data.gis-lab.info/osm_dump/dump/latest/KZ.osm.bz2 
+call getbz2 georgia     http://data.gis-lab.info/osm_dump/dump/latest/GE.osm.bz2
+call getbz2 armenia     http://data.gis-lab.info/osm_dump/dump/latest/AM.osm.bz2
+call getbz2 kyrgyzstan  http://data.gis-lab.info/osm_dump/dump/latest/KG.osm.bz2
+
 call getbz2 azerbaijan  http://data.gis-lab.info/osm/azerbaijan/azerbaijan.osm.bz2
-call getbz2 kazakhstan  http://data.gis-lab.info/osm/kazakhstan/kazakhstan.osm.bz2 
-call getbz2 kyrgyzstan  http://data.gis-lab.info/osm/kyrgyzstan/kyrgyzstan.osm.bz2
+
+
 call getbz2 uzbekistan  http://data.gis-lab.info/osm/uzbekistan/uzbekistan.osm.bz2
 
 rem ================================================================================
@@ -31,8 +34,12 @@ rem ============================================================================
 rem curl ftp://188.40.19.246/osm/dumps/belarus.current.preprocessed.osm.bz2 >D:\OSM\osm_data\belarus.osm.bz2
 rem bunzip2.exe D:\OSM\osm_data\belarus.osm.bz2 --force
 
-curl http://maps-by.googlecode.com/files/belarus.7z >D:\OSM\osm_data\belarus.7z
-7za e D:\OSM\osm_data\belarus.7z -oD:\OSM\osm_data
-del D:\OSM\osm_data\belarus.osm
-rename D:\OSM\osm_data\77777777.osm belarus.osm
+rem 2
+rem curl http://maps-by.googlecode.com/files/belarus.7z >D:\OSM\osm_data\belarus.7z
+rem 7za e D:\OSM\osm_data\belarus.7z -oD:\OSM\osm_data
+rem del D:\OSM\osm_data\belarus.osm
+rem rename D:\OSM\osm_data\77777777.osm belarus.osm
+
+
+call getbz2 belarus http://data.gis-lab.info/osm_dump/dump/latest/BY.osm.bz2
 
