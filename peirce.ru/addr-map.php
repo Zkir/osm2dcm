3cd7b1c8878dd9b94c9f1bca12bfb2bd4917eea3
Zkir <mekillot@yandex.ru>
2011-09-08 19:58:51 +0400

    Фильтрация по типам ошибок - патч от SevenMag

--- peirce.ru/addr-map.php
+++ peirce.ru/addr-map.php
@@ -9,13 +9,27 @@ include("ZSitePage.php");
   $zPage->title="Валидатор адресов";
   $zPage->header="Валидатор адресов";
 
-  $mapid=$_GET['mapid'];
-  $errtype=$_GET['errtype'];
+  $mapid=@$_GET['mapid'];
+  $errtype=@$_GET['errtype'];
 
   $zPage->WriteHtml( "<h1>Валидатор адресов</h1>");
   $zPage->WriteHtml('<p align="right"><a href="/addr.php?mapid='.$mapid.'">Назад к таблице</a> </p>' );
   $zPage->WriteHtml('<p>Отображение на карте пока в тестовом режиме, прошу строго не судить :)</p>');
   $zPage->WriteHtml('<p>По клику на маркере открывается JOSM, он должне быть запущен.</p>');
+  
+  $zPage->WriteHtml('<table width=400><tr><td>Выбор типа ошибок:</td>');
+  if ( $errtype > 0 ) $zPage->WriteHtml("<td><a href='addr-map.php?mapid=$mapid'>Все</a></td>");
+  else $zPage->WriteHtml('<td><b>Все<b></td>');
+  for ( $ii = 1; $ii < 7; $ii++ )
+  {
+    $zPage->WriteHtml('<td>◆</td>');
+    if ($ii <> $errtype)
+      $zPage->WriteHtml("<td><a href='addr-map.php?mapid=$mapid&errtype=$ii' title='".FormatAddrErrName($ii)."'>".FormatAddrErrType($ii).'</a></td>');
+    else
+      $zPage->WriteHtml('<td><b>'.FormatAddrErrType($ii).'</b></td>');
+  }
+  $zPage->WriteHtml("</tr></table>Показаны ошибки типа: <b>".($errtype > 0 ? FormatAddrErrName($errtype) : "Все")."</b>");
+  
   if($mapid!="")
   {
     PrintMap($mapid,$errtype);
@@ -25,44 +39,11 @@ include("ZSitePage.php");
     PrintMap("RU-MOS","");    
   }
 
-
-$zPage->WriteHtml('
-<div style="display: none;">
-<iframe name="josm"></iframe>
-</div>');
-
  $zPage->Output("1");
 
 /* =============================================================================
      Разные полезные фукции
 ===============================================================================*/
-//Имя файла ошибок по коду
-function GetXmlFileName($mapid)
-{
-  return "/ADDR_CHK/".$mapid.".mp_addr.xml";
-}
-
-//Ссылка на Josm
-function MakeJosmLink($lat,$lon)
-{
-  $delta = 0.0001;
-
-  $Link = "http://localhost:8111/load_and_zoom?top=".((float)$lat + $delta)."&bottom=".((float)$lat - $delta)."&left=".((float)$lon - $delta)."&right=".((float)$lon + $delta);
-
-  return $Link;
-}
-
-//Ссылка на Potlatch
-function MakePotlatchLink($lat,$lon)
-{
-  $Link = "http://openstreetmap.org/edit?lat=".$lat."&lon=".$lon."&zoom=17";
-  return $Link;
-}
-
-function MakeWikiLink($city)
-{
-	return "http://ru.wikipedia.org/wiki/".$city;
-}
 
 function PrintMap($mapid,$errtype)
 {
@@ -71,11 +52,11 @@ function PrintMap($mapid,$errtype)
   <div id="cm-example" style="width: 100%; height: 600px"></div> 
   <script type="text/javascript" src="http://tile.cloudmade.com/wml/latest/web-maps-lite.js"></script> 
 	   
-  <script type="text/javascript" src="http://peirce.gis-lab.info/js/addr-map.js"> </script> 
+  <script type="text/javascript" src="./js/addr-map.js"> </script> 
   <script type="text/javascript">
       ProcessMap("ADDR_CHK/'.$mapid.'.mp_addr.xml","'.$errtype.'");
    </script> 
-  <img id="ttt" src="" style="display:none;" alt="dummy link for Josm"/> ');
+  <iframe id="ttt" src="" style="display:none;"></iframe>');
    
   //Классификатор ошибок
   $zPage->WriteHtml( '<h2><a name="errdescr">Объяснение типов ошибок</a></h2>');
@@ -92,184 +73,6 @@ function PrintMap($mapid,$errtype)
 
 }
 	
-//Страница деталей области.
-function PrintAddresses($mapid)
-{
-  global $zPage;
-  $zPage->WriteHtml('<P align=right><a href="/addr.php">Назад к списку регионов</a> <p>' );
-
-  $xml = simplexml_load_file(GetXmlFileName($mapid));
-  $zPage->WriteHtml("<h2>Сводка </h2>" );
-  $zPage->WriteHtml("<table>" );
-  $zPage->WriteHtml("<tr><td>Код карты</td><td><b>".$mapid."</b></td></tr>" );
-  $zPage->WriteHtml("<tr><td>Всего адресов</td><td>".$xml->Summary->TotalHouses."</td></tr>" );
-  $zPage->WriteHtml("<tr><td>Всего улиц</td><td>".$xml->Summary->TotalStreets."</td></tr>" );
-  $zPage->WriteHtml("<tr><td>Не сопоставлено адресов</td><td>".$xml->Summary->UnmatchedHouses."</td></tr>" );
-  $zPage->WriteHtml("<tr><td>Домов без городов</td><td>".$xml->Summary->HousesWOCities."</td></tr>" );
-  $zPage->WriteHtml("<tr><td>Доля несопоставленых адресов</td><td>".number_format(100.00*(float)$xml->Summary->ErrorRate,2,'.', ' ')."%</td></tr>" );
-  $zPage->WriteHtml("</table>" );
-  $zPage->WriteHtml("<p/>" );
-
-/*==========================================================================
-                 Города без полигональных границ
-============================================================================*/
-  $zPage->WriteHtml("<h2>Города без полигональных границ</h2>");
-  $zPage->WriteHtml('<p>Наличие полигональных границ (полигона с place=* и name=* или
-                     place_name=*, такими же, что и на точке города) критически важно
-                     для адресного поиска. По ним определяется принадлежность домов и улиц
-                     населенным пунктам.<br/> В данный список включены города (place=city и place=town),
-                     для которых полигональные границы не обнаружены.
-                     Деревни (place=village|hamlet) в этом списке не показываются,
-                     поскольку деревень может быть очень много, но, если в деревне
-                     есть улицы и дома, полигональные границы так же нужны. Все дома вне НП будут
-                     показаны в  секции "Несопоставленные адреса" ниже.
-                      </p>');
-  $zPage->WriteHtml( '<table width="900px" class="sortable">
-    	    <tr>
-                  <td><b>Название</b></td>
-                  <td width="100px" align="center"><b>Править <br/> в JOSM</b></td>
-                  <td width="100px" align="center"><b>Править <br/>в Potlach</b></td>
-         </tr>');
-
-  foreach ($xml->CitiesWithoutPlacePolygon->City as $item)
-    {
-        $zPage->WriteHtml( '<tr>');
-        $zPage->WriteHtml( '<td>'.$item->City.'</td>');
-        $zPage->WriteHtml( '<td align="center"> <a href="'.MakeJosmLink($item->Coord->lat,$item->Coord->lon).'" target="josm"> <img src="img/josm.png"/></a> </td> ');
-        $zPage->WriteHtml( '<td align="center"> <a href="'.MakePotlatchLink($item->Coord->lat,$item->Coord->lon) .'" target="_blank"><img src="img/potlach.png"/></a> </td> ');
-        $zPage->WriteHtml( '</tr>');
-     }
-  $zPage->WriteHtml( '</table>');
-/*==========================================================================
-                 Города без населения
-============================================================================*/
-  $zPage->WriteHtml("<h2>Города без населения</h2>");
-  $zPage->WriteHtml('<p>Наличие населения (тега population=*) непосредственно на
-                     адресный поиск не влияет. Тем не менее, указание населения крайне
-                     желательно, поскольку от него зависит размер надписи города.
-                      </p>');
-  $zPage->WriteHtml("<p/>" );
-
-  $zPage->WriteHtml( '<table width="900px" class="sortable">
-    	    <tr>
-                  <td><b>Название</b></td>
-                  <td width="100px" align="center"><b>Править <br/> в JOSM</b></td>
-                  <td width="100px" align="center"><b>Править <br/>в Potlach</b></td>
-         </tr>');
-
-  foreach ($xml->CitiesWithoutPopulation->City as $item)
-    {
-        $zPage->WriteHtml( '<tr>');
-        $zPage->WriteHtml( '<td><a href="'.MakeWikiLink($item->City).'" target="_blank">'.$item->City.'</a></td>');
-        $zPage->WriteHtml( '<td align="center"> <a href="'.MakeJosmLink($item->Coord->lat,$item->Coord->lon).'" target="josm"> <img src="img/josm.png"/></a> </td> ');
-        $zPage->WriteHtml( '<td align="center"> <a href="'.MakePotlatchLink($item->Coord->lat,$item->Coord->lon) .'" target="_blank"><img src="img/potlach.png"/></a> </td> ');
-        $zPage->WriteHtml( '</tr>');
-     }
-  $zPage->WriteHtml( '</table>');
-
-/*==========================================================================
-                 Несопоставленные адреса
-============================================================================*/
-  $zPage->WriteHtml("<h2>Несопоставленные адреса </h2>" );
-  $zPage->WriteHtml('<p> Данный валидатор показывает какие дома/адреса <b>не</b>
-                     попадают в адресный поиск после конвертации карт в СитиГид. <br/>
-                     Пустая улица означает, что на доме не заполнено поле addr:street.
-                     Пустой город означает, что дом находится вне полигона place=*.');
-  $zPage->WriteHtml("<p/>" );
-  if ($xml->Summary->HousesWOCities>5000)
-  {
-  	  $zPage->WriteHtml( '<p><b>К сожалению, ошибок настолько много, что отобразить их все невозможно. 
-  	                      Следует сначала починить полигональные границы городов.</b></p>');
-  }
-  else
-  {	  
-  $zPage->WriteHtml( '<P>И, между прочим, таблица сортируется. Нужно кликнуть на заголовок столбца. </P> ');
-
-
-  $zPage->WriteHtml( '<table width="900px" class="sortable">
-
-   	    <tr>
-                  <td><b>Город</b></td>
-                  <td><b>Улица</b></td>
-                  <td><b>Номер дома </b></td>
-                  <td width="100px" align="center"><b>Править <br/> в JOSM</b></td>
-                  <td width="100px" align="center"><b>Править <br/>в Potlach</b></td>
-         </tr>');
-
-  foreach ($xml->ErrorList->House as $item)
-    {
-        $zPage->WriteHtml( '<tr>');
-        $zPage->WriteHtml( '<td>'.$item->City.'</td>');
-        $zPage->WriteHtml( '<td>'.$item->Street.'</td>');
-        $zPage->WriteHtml( '<td>'.$item->HouseNumber.'</td>');
-        if ($item->JosmLink<>"")
-        {
-          $zPage->WriteHtml( '<td> <a href="'.$item->JosmLink.'" target="josm"> <img src="img/josm.png"/></a> </td> ');
-          $zPage->WriteHtml( '<td> - </td> ');
-        }
-        else
-        {
-          $zPage->WriteHtml( '<td align="center"> <a href="'.MakeJosmLink($item->Coord->lat,$item->Coord->lon).'" target="josm"> <img src="img/josm.png"/></a> </td> ');
-          $zPage->WriteHtml( '<td align="center"> <a href="'.MakePotlatchLink($item->Coord->lat,$item->Coord->lon) .'" target="_blank"><img src="img/potlach.png"/></a> </td> ');
-        }
-        $zPage->WriteHtml( '</tr>');
-     }
-
-  $zPage->WriteHtml( '</table>');
-  }
-}
-
-function PrintAddressesSummary()
-{
-   global $zPage;
-
-   //Cписок пока строим по статистике
-   $xml = simplexml_load_file("statistics.xml");
-
-   $zPage->WriteHtml( '<table width="900px" class="sortable">
-
-   	    <tr>
-                  <td width="80px"><b>Код</b></td>
-                  <td width="180px"><b>Карта</b></td>
-                  <td><b>Всего<br/> адресов</b></td>
-                  <td><b>Всего<br/> улиц</b></td>
-                  <td><b>Не сопоставлено<br/> адресов</b></td>
-                  <td><b>Домов <br/> вне НП</b></td>
-                  <td><b>Доля<br/> битых<br/> адресов, %</b></td>
-                  <td><b>Ошибки</b></td>
-
-                 <!-- <td><b>Дата</b></td> -->
-         </tr>');
-
-  foreach ($xml->mapinfo as $item)
-    {
-      if(  substr($item->MapId,0,2)=='RU' )
-      {
-        $xmlfilename=GetXmlFileName($item->MapId);
-
-        if(file_exists($xmlfilename))
-        {
-          $xml_addr = simplexml_load_file($xmlfilename);
-          $zPage->WriteHtml( '<tr>');
-          $zPage->WriteHtml( '<td>'.$item->MapId.'</td>');
-          $zPage->WriteHtml( '<td>'.$item->MapName.'</td>');
-          $zPage->WriteHtml( '<td>'.$xml_addr->Summary->TotalHouses.'</td>' );
-          $zPage->WriteHtml( '<td>'.$xml_addr->Summary->TotalStreets.'</td>' );
-          $zPage->WriteHtml( '<td>'.$xml_addr->Summary->UnmatchedHouses.'</td>');
-          $zPage->WriteHtml( '<td>'.$xml_addr->Summary->HousesWOCities.'</td>' );
-          $zPage->WriteHtml( '<td>'.number_format(100.00*(float)$xml_addr->Summary->ErrorRate,2,'.', ' ').'</td>');
-          $zPage->WriteHtml( '<td><a href="/addr.php?mapid='.$item->MapId.'">посмотреть</a></td>');
-
-          $zPage->WriteHtml( '</tr>');
-        }
-
-
-
-      }
-    }
-
-  $zPage->WriteHtml( '</table>');
-}
 
 function FormatAddrErrType($number)
 {
