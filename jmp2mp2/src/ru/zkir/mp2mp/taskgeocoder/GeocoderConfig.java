d30593c272ff0e98331faa1d9dd3e8c239a9df27
Zkir <mekillot@yandex.ru>
2014-05-24 00:10:39 +0400

    "Лишние слова", Аргентина

--- jmp2mp2/src/ru/zkir/mp2mp/taskgeocoder/GeocoderConfig.java
+++ jmp2mp2/src/ru/zkir/mp2mp/taskgeocoder/GeocoderConfig.java
@@ -480,6 +480,8 @@ public class GeocoderConfig{
     {
       levelsForCity=new String[] {CITY_POLYGON, ADMIN_LEVEL_8, NEAREST_CITY_POINT};
       levelsForRegion=new String[] {ADMIN_LEVEL_4};
+      redundantWords =new String[]{"Ciudad Autonoma de","Ciudad de"};
+
     }
 
     //Колумбия
